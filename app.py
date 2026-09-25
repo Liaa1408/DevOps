ce97b22564ff67279d175d233e3193cde3f4029f
Liaa1408 <sandelasuhitha@gmail.com>
2026-09-25 21:59:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 #feature_1 update
+#feature_ remote update
 @app.route("/", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
